c119256a1c75812460121c275e227ec8cb079754
Neon-jeff <omaksteejay@gmail.com>
2023-07-29 11:34:48 +0100

    Static configfiles

--- core/settings.py
+++ core/settings.py
@@ -144,7 +144,7 @@ COMPRESS_ROOT = BASE_DIR / 'static'
 
 # STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles', 'static')
 MEDIA_URLS ='/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
